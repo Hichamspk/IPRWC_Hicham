50d3684fc1b6ed8eafc948123853bdc3a90ebf5e
Hicham <s1143822@student.hsleiden.nl>
2024-01-05 11:02:57 +0100

    FIX: changed product price to Double

--- src/main/java/com/iprwc2/model/Product.java
+++ src/main/java/com/iprwc2/model/Product.java
@@ -21,8 +21,7 @@ public class Product {
     private String name;
     private String category;
     private String description;
-    private BigDecimal price;
-    private int stock;
+    private Double price;
     private String imageUrl;
 
 }
--- src/main/java/com/iprwc2/seeder/ProductSeeder.java
+++ src/main/java/com/iprwc2/seeder/ProductSeeder.java
@@ -18,11 +18,11 @@ public class ProductSeeder implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         // Voeg hier producten toe
-        Product product1 = new Product(null, "Kite", "HunterxHunter", "Kite funko pop", new BigDecimal("15.00"), 15, "https://funkoeurope.com/cdn/shop/products/61378_HunterxHunter_Kite_POP_GLAM-WEB_600x.png?v=1652883795");
-        Product product2 = new Product(null, "Product 2", "OnePiece", "Marco funko pop", new BigDecimal("3.50"), 20, "https://funkoeurope.com/cdn/shop/files/74474-POP-Animation-One-Piece---Marco_GLAM-WEB_600x.png?v=1694609080");
-        Product product3 = new Product(null, "Product 3", "Categorie 3", "Beschrijving 3", new BigDecimal("5.99"), 5, "image_url");
-        Product product4 = new Product(null, "Product 4", "Categorie 4", "Beschrijving 4", new BigDecimal("15.00"), 1, "image_url");
-        Product product5 = new Product(null, "Product 5", "Categorie 5", "Beschrijving 5", new BigDecimal("2.50"), 60, "image_url");
+        Product product1 = new Product(null, "Kite", "HunterxHunter", "Kite funko pop", new Double("15.00"),  "https://funkoeurope.com/cdn/shop/products/61378_HunterxHunter_Kite_POP_GLAM-WEB_600x.png?v=1652883795");
+        Product product2 = new Product(null, "Product 2", "OnePiece", "Marco funko pop", new Double("3.50"),  "https://funkoeurope.com/cdn/shop/files/74474-POP-Animation-One-Piece---Marco_GLAM-WEB_600x.png?v=1694609080");
+        Product product3 = new Product(null, "Product 3", "Categorie 3", "Beschrijving 3", new Double("5.99"),  "image_url");
+        Product product4 = new Product(null, "Product 4", "Categorie 4", "Beschrijving 4", new Double("15.00"),  "image_url");
+        Product product5 = new Product(null, "Product 5", "Categorie 5", "Beschrijving 5", new Double("2.50"),  "image_url");
 
 
 
